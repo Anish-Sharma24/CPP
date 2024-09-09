753f0e4f4a51368d927584c5fc0c087c857824fb
Anish-Sharma24 <learninghowtoprogramme@gmail.com>
2024-09-09 08:29:37 +0530

    numbers_printing

--- new.cpp
+++ new.cpp
@@ -2,5 +2,6 @@
 int main() // main function. Function in c++ is a block of code that does many things.
 {
     std:: cout << "Hello World ! This is my first C++ project" << std:: endl;
+    std:: cout <<"This is Anish Sharma";
     return 0;
 }
---
+++ numbersprinting.cpp
@@ -0,0 +1,9 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Number One" << endl;
+    cout << "Number Two" << endl;
+    cout << "Number Three" << endl;
+    return 0;
+}
